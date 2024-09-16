ab5260bd72e7f67be4dfcbef243f54055b8be3e1
Sebastian <s.martineza23@uniandes.edu.co>
2024-09-15 20:48:11 -0500

    Medico Entity

---
+++ src/main/java/co/edu/uniandes/dse/parcialejemplo/entities/MedicoEntity.java
@@ -0,0 +1,11 @@
+package co.edu.uniandes.dse.parcialejemplo.entities;
+
+import javax.persistence.Entity;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class MedicoEntity extends BaseEntity {
+    
+}
